d9babaab3c38f206c2496b15df54552f6aa0030c
travis88 <hellsballs99@gmail.com>
2018-05-17 05:33:10 +0300

    adding users

--- api/views.py
+++ api/views.py
@@ -11,6 +11,7 @@ import status
 api_bp = Blueprint('api', __name__)
 category_schema = CategorySchema()
 message_schema = MessageSchema()
+user_schema = UserSchema()
 api = Api(api_bp)
 auth = HTTPBasicAuth()
 
@@ -27,10 +28,66 @@ def verify_user_password(name, password):
 
 class AuthRequiredResource(Resource):
     """Авторизация"""
+
     method_decorators = [auth.login_required]
 
 
-class MessageResource(Resource):
+class UserResource(AuthRequiredResource):
+    """Ресурс пользователь"""
+
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        result = user_schema.dump(user).data
+        return result
+
+
+class UserListResource(Resource):
+    """Ресурс списка пользователей"""
+
+    @auth.login_required
+    def get(self):
+        """Получение пользователя"""
+        pagination_helper = PaginationHelper(
+            request,
+            query=User.query,
+            resource_for_url='api.userlistresource',
+            key_name='results',
+            schema=user_schema)
+        result = pagination_helper.paginate_query()
+        return result
+    
+    def post(self):
+        """Добавление пользователя"""
+        request_dict = request.get_json()
+        if not request_dict:
+            response = {'user': 'No input data provided'}
+            return response, status.HTTP_400_BAD_REQUEST
+        errors = user_schema.validate(request_dict)
+        if errors:
+            return errors, status.HTTP_400_BAD_REQUEST
+        name = request_dict['name']
+        existing_user = User.query.filter_by(name=name).first()
+        if existing_user is not None:
+            response = {'user': 'An user with the same nama already exists'}
+            return response, status.HTTP_400_BAD_REQUEST
+        try:
+            user = User(name=name)
+            error_message, password_ok = \
+            user.check_password_strength_and_hash_if_ok(request_dict['password'])
+            if password_ok:
+                user.add(user)
+                query = User.query.get(user.id)
+                result = user_schema.dump(query).data
+                return result, status.HTTP_201_CREATED
+            else:
+                return {'error': error_message}, status.HTTP_400_BAD_REQUEST
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            resp = {'error': str(e)}
+            return resp, status.HTTP_400_BAD_REQUEST
+
+
+class MessageResource(AuthRequiredResource):
     """Ресурс сообщение"""
 
     def get(self, id):
@@ -85,7 +142,7 @@ class MessageResource(Resource):
             return resp, status.HTTP_401_UNAUTHORIZED
 
 
-class MessageListResource(Resource):
+class MessageListResource(AuthRequiredResource):
     """Список сообщений"""
 
     def get(self):
@@ -133,7 +190,7 @@ class MessageListResource(Resource):
             return resp, status.HTTP_400_BAD_REQUEST
 
 
-class CategoryResource(Resource):
+class CategoryResource(AuthRequiredResource):
     """Категория"""
 
     def get(self, id):
@@ -181,7 +238,7 @@ class CategoryResource(Resource):
             return resp, status.HTTP_401_UNAUTHORIZED
 
 
-class CategoryListResource(Resource):
+class CategoryListResource(AuthRequiredResource):
     """Список категорий"""
 
     def get(self):
@@ -219,3 +276,5 @@ api.add_resource(CategoryListResource, '/categories/')
 api.add_resource(CategoryResource, '/categories/<int:id>')
 api.add_resource(MessageListResource, '/messages/')
 api.add_resource(MessageResource, '/messages/<int:id>')
+api.add_resource(UserListResource, '/users/')
+api.add_resource(UserResource, '/users/<int:id>')
