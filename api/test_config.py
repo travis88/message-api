5c52273aa399faedd113afea373abfc9af3e92dc
travis88 <hellsballs99@gmail.com>
2018-05-18 06:29:11 +0300

    tests added

--- api/test_config.py
+++ api/test_config.py
@@ -1,4 +1,3 @@
-"""Конфиг"""
 import os
 
 
@@ -9,7 +8,9 @@ HOST = '127.0.0.1'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "{0}{1}".format(os.environ.get('MESSAGES_DB_URL'),
-                                          "messages")
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+                                          "test_messages")
+TESTING = True
+SERVER_NAME = '127.0.0.1:5000'                        
 PAGINATION_PAGE_SIZE = 5
 PAGINATION_PAGE_ARGUMENT_NAME = 'page'
+WTF_CSRF_ENABLED = False
